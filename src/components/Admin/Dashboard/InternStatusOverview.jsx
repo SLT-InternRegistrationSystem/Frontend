567c695e52dd0ff53821efe211db50d854df304f
Sandani Gunawardhana <sandanigunawardhana@gmail.com>
2025-01-09 10:32:56 +0530

    admin dashboard and new applications pages

---
+++ src/components/Admin/Dashboard/InternStatusOverview.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import ReactApexChart from 'react-apexcharts';
+
+const InternStatusOverview = () => {
+    const totalInterns = 44 + 55 + 13 + 43;
+
+    const [state] = useState({
+        series: [58, 55, 5, 18],
+        options: {
+            chart: {
+                width: 500,
+                type: 'donut',
+            },
+            labels: ['Active', 'Completed', 'Terminated', 'Dropout'],
+            colors: ['#aaeba7', '#84bff5', '#fa7f7f', '#ffe78f'], 
+            plotOptions: {
+                pie: {
+                    donut: {
+                        labels: {
+                            show: true,
+                            total: {
+                                show: true,
+                                formatter: () => totalInterns.toString(), 
+                                label: 'Total Interns',
+                            },
+                        },
+                    },
+                },
+            },
+            dataLabels: {
+                enabled: true,
+                formatter: (val, opts) => {
+                    const index = opts.seriesIndex;
+                    return `${opts.w.config.series[index]}`; 
+                },
+            },
+            responsive: [
+                {
+                    breakpoint: 480,
+                    options: {
+                        chart: {
+                            width: 250,
+                        },
+                        legend: {
+                            position: 'bottom',
+                        },
+                    },
+                },
+            ],
+        },
+    });
+
+    return (
+        <div>
+            <div id="chart">
+                <ReactApexChart
+                    options={state.options}
+                    series={state.series}
+                    type="donut"
+                    width={350}
+                />
+            </div>
+            
+            <style>{`
+                .apexcharts-datalabel-label {
+                    font-size: 13px !important; /* Label font size */
+                    font-weight: 600 !important; /* Label font weight */
+                    fill: #a9a9a9 !important;    /* Label text color */
+                }
+                .apexcharts-datalabel-value {
+                    font-size: 30px !important; /* Total value font size */
+                    font-weight: bold !important; /* Total value font weight */
+                    fill: #4b5563 !important;    /* Total value color */
+                }
+            `}</style>
+        </div>
+    );
+};
+
+export default InternStatusOverview;
---
+++ src/components/Admin/Dashboard/SpecializationWiseInternDistribution.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import ReactApexChart from 'react-apexcharts';
+
+const SpecializationWiseInternDistribution = () => {
+    const [state, setState] = React.useState({
+          
+        series: [{
+          name: 'Dropout',
+          data: [3, 2, 0, 0, 5, 2]
+        }, {
+          name: 'Terminated',
+          data: [3, 3, 0, 4, 3, 1]
+        }, {
+          name: 'Completed',
+          data: [11, 17, 15, 15, 16, 17]
+        }, {
+          name: 'Active',
+          data: [21, 7, 20, 13, 15, 12]
+        }],
+        options: {
+          chart: {
+            type: 'bar',
+            height: 300,
+            stacked: true,
+            toolbar: {
+              show: true
+            },
+            zoom: {
+              enabled: true
+            }
+          },
+          responsive: [{
+            breakpoint: 480,
+            options: {
+              legend: {
+                position: 'bottom',
+                offsetX: -10,
+                offsetY: 0
+              }
+            }
+          }],
+          plotOptions: {
+            bar: {
+              horizontal: false,
+              columnWidth: '60%',
+              borderRadius: 3,
+              borderRadiusApplication: 'end', 
+              borderRadiusWhenStacked: 'last',
+              dataLabels: {
+                total: {
+                  enabled: true,
+                  style: {
+                    fontSize: '13px',
+                    fontWeight: 900
+                  }
+                }
+              }
+            },
+          },
+          colors: ['#ffe78f', '#fa7f7f', '#84bff5', '#aaeba7'],
+          xaxis: {
+            type: 'string',
+            categories: ['Fullstack', 'Cloud', 'UI/UX', 'QA', 'PM', 'BA'
+            ],
+          },
+          legend: {
+            position: 'right',
+            offsetY: 40,
+            markers: {
+                width: 10, 
+                height: 10, 
+                radius: 12, 
+                shape: 'circle',
+            },
+          },
+          fill: {
+            opacity: 1
+          }
+        },
+    });
+
+    
+        return (
+            <div>
+            <div id="chart">
+                <ReactApexChart options={state.options} series={state.series} type="bar" height={229} width={700} />
+              </div>
+            <div id="html-dist"></div>
+          </div>
+        );
+}
+
+export default SpecializationWiseInternDistribution
